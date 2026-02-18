c011bafd60782fd71cc9809c92dc7732d4f70d4e
ArlenATU <G00470377@atu.ie>
2026-02-18 15:36:37 +0000

    init commit

---
+++ src/main/java/atu/ie/oop2_week3/Oop2Week3Application.java
@@ -0,0 +1,13 @@
+package atu.ie.oop2_week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop2Week3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oop2Week3Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/oop2_week3/Oop2Week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.oop2_week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2Week3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
